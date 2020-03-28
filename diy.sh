5d8a0b5b376738410b1631b27eb4647b9ee6b6a0
xingelai <57555837+xingelai@users.noreply.github.com>
2020-03-28 13:23:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Description: yigeer
+# Lisence: yigeer
+# Author: yigeer
+# Blog: 
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.44.1/g' package/base-files/files/bin/config_generate
