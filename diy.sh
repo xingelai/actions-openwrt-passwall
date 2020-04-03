ff8d2e3eb0f8ddd714f46f18ad058578e0a67435
xingelai <57555837+xingelai@users.noreply.github.com>
2020-04-03 14:13:54 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,4 @@
 # Blog: 
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.44.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.5/g' package/base-files/files/bin/config_generate
